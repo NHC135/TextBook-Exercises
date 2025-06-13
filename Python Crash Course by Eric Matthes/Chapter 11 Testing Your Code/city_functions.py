095864b5e2a382e3bb34fb73de34085964216f2d
NHC135 <nathaniel.choi123@gmail.com>
2025-06-13 11:14:17 -0400

    Add files via upload

---
+++ Python Crash Course by Eric Matthes/Chapter 11 Testing Your Code/city_functions.py
@@ -0,0 +1,6 @@
+#11-1. City, Country
+
+def test_city_country(city, country): 
+    """verifies that the city, country results in the correct string."""
+    return city.title() + ' ' + country.title()
+
