fd02b37830403033122cb1760013fff004d97a80
NHC135 <nathaniel.choi123@gmail.com>
2025-06-27 01:23:59 -0400

    Add files via upload

---
+++ SQL Exercises/Stored Procedure/ERROR_HANDLING.sql
@@ -0,0 +1,56 @@
+--ERROR	Handling
+ 
+EXEC GetCustomerSummary
+
+ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA'
+AS
+BEGIN
+
+	DECLARE @TotalCustomers INT, @AvgScore FLOAT;
+
+	SELECT 1 FROM Sales.Customers WHERE Score IS NULL AND Country = 'Germany' 
+	IF EXISTS (SELECT 1 FROM Sales.Customers WHERE Score IS NULL AND Country = @Country)
+		BEGIN 
+			PRINT('Updating NULL Scores to 0') 
+			UPDATE Sales.Customers
+			SET Score = 0
+			WHERE Score IS NULL AND Country = @Country;
+		END
+
+		ELSE
+		BEGIN
+			PRINT('No NULL Scores found')
+		END;
+
+		SELECT 
+			@TotalCustomers = COUNT(CustomerID),
+			@AvgScore = AVG(Score)
+		FROM Sales.Customers
+		WHERE Country = @Country;
+
+		PRINT 'Total Customers from '+ @Country + ':' + CAST(@TotalCustomers AS NVARCHAR)
+		PRINT 'Average Score from ' + @Country + ':' + CAST(@AvgScore AS NVARCHAR) 
+
+
+		SELECT 
+			COUNT(OrderID) AS Nr_of_Orders,
+			SUM(Sales) AS Total_Sales,
+			1/0 --Error
+		FROM Sales.Orders AS o
+		JOIN Sales.Customers AS c
+		ON o.CustomerID = c.CustomerID
+		WHERE c.Country = @Country;
+
+	END TRY 
+	BEGIN CATCH
+		PRINT('An error has occurred');
+		PRINT('ERROR message: ' + ERROR_MESSAGE()); 
+		PRINT('ERROR Number: ' +CAST (ERROR_NUMBER() AS NVARCHAR));
+		PRINT('ERROR Line: ' +CAST(ERROR_LINE() AS NVARCHAR));
+		PRINT('ERROR Procedure: ' + ERROR_PROCEDURE());
+	END CATCH 
+END 
+GO
+
+EXEC GetCustomerSummary @Country = 'Germany'
+ 
